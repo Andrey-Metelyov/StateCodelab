e731c16b4c1a8c38459c377891408fd99d148c38
Andrey.Metelyov <andrey.metelyov@gmail.com>
2021-10-04 15:02:58 +0900

    Define editor state

--- start/src/main/java/com/codelabs/state/todo/TodoViewModel.kt
+++ start/src/main/java/com/codelabs/state/todo/TodoViewModel.kt
@@ -16,13 +16,21 @@
 
 package com.codelabs.state.todo
 
+import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateListOf
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.setValue
 import androidx.lifecycle.ViewModel
 
 class TodoViewModel : ViewModel() {
 
+    private var currentEditPosition by mutableStateOf(-1)
+
     var todoItems = mutableStateListOf<TodoItem>()
         private set
+
+    val currentEditItem: TodoItem?
+        get() = todoItems.getOrNull(currentEditPosition)
 
     fun addItem(item: TodoItem) {
         todoItems.add(item)
